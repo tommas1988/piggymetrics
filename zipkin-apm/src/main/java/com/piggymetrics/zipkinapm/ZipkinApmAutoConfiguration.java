0db8cd0a36406aecd47da56441eea6d0927b5f1c
tommas <tommas1988@live.com>
2021-04-12 23:47:01 +0800

    remove sleuth

--- zipkin-apm/src/main/java/com/piggymetrics/zipkinapm/ZipkinApmAutoConfiguration.java
+++ zipkin-apm/src/main/java/com/piggymetrics/zipkinapm/ZipkinApmAutoConfiguration.java
@@ -1,23 +1,44 @@
 package com.piggymetrics.zipkinapm;
 
+import brave.httpclient.TracingHttpClientBuilder;
+import com.mongodb.MongoClient;
 import com.mongodb.MongoClientOptions;
+import org.apache.http.client.HttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
 import org.springframework.beans.factory.support.BeanDefinitionRegistry;
-import org.springframework.boot.autoconfigure.AutoConfigureOrder;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.*;
-import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
 import org.springframework.core.type.AnnotationMetadata;
 
 @Configuration
 @Import(ZipkinApmAutoConfiguration.BeanPostProcessorsRegistrar.class)
-@AutoConfigureOrder(Ordered.LOWEST_PRECEDENCE)
+// Lowest precedence order
+@Order
 public class ZipkinApmAutoConfiguration {
     @Bean
+    @ConditionalOnClass(MongoClient.class)
     @ConditionalOnMissingBean(type = "com.mongodb.MongoClientOptions")
+    // TODO: find a way to merge multiple bean
     public MongoClientOptions mongoClientOptions() {
         return MongoClientOptions.builder()
                 .addCommandListener(MongoZipkinApmConfiguration.getCommandListener())
                 .build();
+    }
+
+    @Bean
+    @ConditionalOnClass(HttpClient.class)
+    @ConditionalOnBean(type = "org.apache.http.impl.client.HttpClientBuilder")
+    public HttpClientBuilder apacheHttpClientBuilder() {
+        return TracingHttpClientBuilder.create()
+        return HttpClientBuilder.create();
+    }
+
+    @ConditionalOnBean(type = "org.apache.http.impl.client.HttpClientBuilder")
+    public void configApacheHttpClientBuilder(HttpClientBuilder builder) {
+
     }
 
     public static class BeanPostProcessorsRegistrar implements ImportBeanDefinitionRegistrar {
