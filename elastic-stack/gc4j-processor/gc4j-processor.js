84a06111fe783086876c04ba22066f4a7301796c
Tommas Yuan <tommas1988@live.com>
2021-06-01 16:43:53 +0800

    gc4j-processor

--- elastic-stack/gc4j-processor/gc4j-processor.js
+++ elastic-stack/gc4j-processor/gc4j-processor.js
@@ -1,8 +1,25 @@
-function process(event) {
-  var pi = new ParseInfo(event.Get('message'));
+var debug = false;
 
-  parseDateStamp(event, pi);
-  parseTimeStamp(event, pi);
+function register(params) {
+  debug = !!(params.debug);
+}
+
+function process(event) {
+  var parseInfo = new ParseInfo(event.Get('message'));
+  var gcEvent = new GCEvent();
+
+  parseDateStamp(parseInfo, gcEvent);
+  parseTimeStamp(parseInfo, gcEvent);
+
+  if (!gcEvent.timestamp) {
+      if (debug)
+        throw 'Can not parse gc timestamp at pos: ' + parseInfo.pos;
+
+    // no gc timestamp find skip this event
+    return;
+  }
+
+
 }
 
 function ParseInfo(message) {
@@ -14,15 +31,22 @@ ParseInfo.prototype.equalsAt(pos, str) {
   return this.message.substring(pos, pos + str.length) == str;
 }
 
+function GCEvent() {
+  this.datestamp = null;
+  this.timestamp = null;
+  this.gcType = null;
+}
+
 // find and convert 2021-05-31T22:05:40.007+0800 to 2021-05-31T22:05:40.007Z for @timestamp
 var LENGTH_OF_DATESTAMP = 28;
 
-function parseDateStamp(event, pi) {
+function parseDateStamp(pi, gcEvent) {
   if (pi.message[4] == '-' && pi.message[7] == '-') {
     // TODO: validate the date with a regular expression
     var date = new Date(pi.message.substring(0, LENGTH_OF_DATESTAMP));
     if (date && pi.equalsAt(LENGTH_OF_DATESTAMP, ': ')) {
-      event.Put('@timestamp', pi.message.substring(0, LENGTH_OF_DATESTAMP - 5) + 'Z');
+      gcEvent.datestamp = pi.message.substring(0, LENGTH_OF_DATESTAMP - 5) + 'Z';
+
       // pass the following ": "
       pi.pos = LENGTH_OF_DATESTAMP + 2
     }
@@ -33,13 +57,20 @@ var timeStampRegExp = /^\d*\.\d/;
 // enough to hold to timestamp string
 var LENGTH_OF_TIMESTAMP = 20;
 
-function parseTimeStamp(event, pi) {
+function parseTimeStamp(pi) {
   var tsStr = pi.message.substring(pi.pos, pi.pos + LENGTH_OF_TIMESTAMP);
   var r = timeStampRegExp.exec(tsStr);
   var ts;
   if (r && (ts = r[0]) && pi.equalsAt(pi.pos + ts.length, ': ')) {
-    event.Put('gc.timestamp', ts);
+    gcEvent.timestamp = ts;
+
     // pass the following ": "
     pi.pos += (ts.length + 2)
   }
+
+
+}
+
+function parseGCEvent(pi, gcEvent) {
+
 }
